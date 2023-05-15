5c320dadc5f0dd01515c73cf8a8c3f5becf9e18b
UriasDR <129330378+UriasDR@users.noreply.github.com>
2023-05-15 14:48:39 -0300

    Update autoscaling.tf

--- terraform-provisioning/autoscaling.tf
+++ terraform-provisioning/autoscaling.tf
@@ -3,32 +3,58 @@ resource "aws_launch_configuration" "my_launch_configuration" {
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.my_security_group.id]
   key_name        = "SSH"
-  user_data       = <<-EOF
+
+  user_data = <<-EOF
               #!/bin/bash
-              sudo yum update -y
-              sudo yum upgrade -y
-              sudo yum install -y git
-              sudo yum -y install docker
-              sudo service docker start
-              sudo usermod -a -G docker ec2-user
-              sudo systemctl enable docker
-              sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-              sudo chmod +x /usr/local/bin/docker-compose
-              sudo yum install amazon-efs-utils -y
-              sudo systemctl start efs && sudo systemctl enable efs
-              sudo mkdir /efs
+              yum update -y
+              yum upgrade -y
+              yum install docker -y
+              usermod -a -G docker ec2-user
+              systemctl start docker && systemctl enable docker
+              curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+              chmod +x /usr/local/bin/docker-compose
+              yum install amazon-efs-utils -y
+              systemctl start efs && systemctl enable efs
+              mkdir /efs
               cd /
-              sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_mount_target.efs_mount_target_a.ip_address}:/ /efs
-              sudo echo 'AllowUsers ec2-user'
-              sudo echo ${aws_efs_mount_target.efs_mount_target_a.ip_address}:/ /efs nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,_netdev 0 0 | sudo tee -a /etc/fstab
-              git clone  https://github.com/UriasDR/Atividade_AWS_Docker.git
-              mv Atividade_AWS_Docker /efs
+              mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport ${aws_efs_mount_target.efs_mount_target_a.ip_address}:/ /efs
+              echo ${aws_efs_mount_target.efs_mount_target_a.ip_address}:/ /efs nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,_netdev 0 0 | sudo tee -a /etc/fstab
               cd /efs
               mkdir db_data && mkdir wp_data
-              cd Atividade_AWS_Docker
-              sudo echo "${aws_db_instance.my_db_instance.endpoint}" > endpoint.txt
-              docker-compose up        
+              echo '
+version: "3"
+services:
+  wordpress:
+    image: wordpress:latest
+    ports:
+      - 80:80
+    restart: always
+    environment:
+      - WORDPRESS_DB_HOST=${aws_db_instance.my_db_instance.endpoint}
+      - WORDPRESS_DB_USER=admin
+      - WORDPRESS_DB_PASSWORD=wordpress
+      - WORDPRESS_DB_NAME=wordpress
+    volumes:
+      - /efs/wp_data:/var/www/html
+  db:
+    image: mysql:latest
+    volumes:
+      - /efs/db_data:/var/lib/mysql
+    restart: always
+    environment:
+      - MYSQL_DATABASE=wordpress
+      - MYSQL_USER=admin
+      - MYSQL_PASSWORD=wordpress
+      - MYSQL_ROOT_PASSWORD=wordpress
+      - MYSQL_HOST=${aws_db_instance.my_db_instance.endpoint}
+      - MYSQL_PORT=3306
+volumes:
+  wp_data:
+  db_data:' > compose.yaml
+              docker-compose up -d 
               EOF
+
+  depends_on = [aws_db_instance.my_db_instance]
 }
 resource "aws_autoscaling_group" "my_autoscaling_group" {
   launch_configuration = aws_launch_configuration.my_launch_configuration.id
